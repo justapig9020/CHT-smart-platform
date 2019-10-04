678ab92ba0d94ad981a302f45221f7fa0e1a7bda
justapig9020 <justapig9020@gmail.com>
2019-10-04 21:14:10 +0800

    Runable version
    Function all green:
        - Select data source
        - Read data
    
    Test enviroment:
        One by two matrex, only one row
    
    Todo:
        Test the wifi module

--- end-device/end-device.ino
+++ end-device/end-device.ino
@@ -18,6 +18,7 @@
 #define SEL 12 // Pin of select
 #define DATA 2 // Last pin of data, the n wide data line must be wired from 2+n-1 to 2 (inverse the order)
 #define WIDE 4 // The wide of data bus
+#define FREQ 1 // The frequence of clock
 //#define DEBUG  // Set the debug mode (Modify DEBUG to _DEBUG to disable the debug functions)
 
 /* ================= */
@@ -72,11 +73,13 @@ inline void clk_off()
 }
 */
 
+// Select ON 
 inline void sel_on()
 {
     digitalWrite (SEL, 1);
 }
 
+//Select OFF
 inline void sel_off()
 {
     digitalWrite (SEL, 0);
@@ -85,8 +88,9 @@ inline void sel_off()
 inline void exe_clk()
 {   
     digitalWrite (CLK, 1);
-    delay(cyc);
+    delay (cyc);
     digitalWrite (CLK, 0);
+    delay (cyc);
 }
 
 // To initialize the size of tile
@@ -105,7 +109,7 @@ char *init_size()
 void set_clk (int t)
 {
     clk = t;
-    wait = 1.0/clk * 100;
+    cyc = (int)(1.0/clk * 1000)>>1;
 }
 
 // Must to be modify if data wide over 8 bits
@@ -159,24 +163,38 @@ void setup()
         pinMode (DATA+i, INPUT);
     Serial.begin (BAUD);
     tile = init_size ();
-    set_clk (1);
+    set_clk (FREQ);
     Serial.println ("Initialized!");
-    Serial.println ("Clock: " + clk + " HZ");
-    Serial.println ("Tile size: " + r_size + " * " + c_size);
+    Serial.print ("Clock: ");
+    Serial.print (clk);
+    Serial.println (" HZ");
+    Serial.print ("Tile size: ");
+    Serial.print (r_size);
+    Serial.print (" * ");
+    Serial.println (c_size);
+    Serial.println (cyc);
 }
 
 void loop() 
 {
+    Serial.println ("\n==== Loop ====");
     sel_on ();
     exe_clk ();
     sel_off ();
+    int d;
     for (int i=0; i<r_size; i++) {
-        for (int o=0; o<c_size; o+) {
+        for (int o=0; o<c_size; o++) {
             if (i & 0x1)
-                tile [i][c_size-o] = get_data ();
+                d = *(tile+i*c_size+c_size-o) = get_data ();
             else
-                tile [i][o] = get_data ();
+                d = *(tile+i*c_size+c_size) = get_data ();
             exe_clk ();
+            Serial.print ("data ");
+            Serial.print (i);
+            Serial.print (" * ");
+            Serial.print (o);
+            Serial.print (": ");
+            Serial.println (d);
         }
     }
     send_data ();
