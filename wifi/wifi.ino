688404feb070e4ed0c98f7d496ae1a82d8488108
justapig9020 <justapig9020@gmail.com>
2019-10-19 15:40:04 +0800

    wifi arduino communication test

--- wifi/wifi.ino
+++ wifi/wifi.ino
@@ -1,24 +1,20 @@
 #include <ESP8266WiFi.h>
 void setup() {
   Serial.begin(115200);
-  WiFi.begin("wifi_test", "qweasdzxc");
+  WiFi.begin("prlab5G", "csc0123456789");
 
-  /*
-   *  若要指定IP位址，請自行在此加入WiFi.config()敘述。
-   WiFi.config(IPAddress(192,168,1,50),    // IP位址
-               IPAddress(192,168,1,1),     // 閘道（gateway）位址
-               IPAddress(255,255,255,0));  // 網路遮罩（netmask）
-   */
-  Serial.println ("Connecting");
+  //Serial.println ("Connecting");
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.println ("ReConnecting");
+    //Serial.println ("ReConnecting");
     delay(500);
   }
-  Serial.println("");
-  Serial.println("IP address: ");
-  Serial.print(WiFi.localIP());
-  Serial.println("WiFi status:");
-  WiFi.printDiag(Serial);
+  //Serial.println("");
+  //Serial.println("IP address: ");
+  //Serial.print(WiFi.localIP());
+  //Serial.println("WiFi status:");
+  //WiFi.printDiag(Serial);
+  Serial.print('c');
 }
 void loop() {
+  
 }
